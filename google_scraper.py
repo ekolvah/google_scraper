bb4cad89d56abcac3eb8df10dafcdeb13618c3d8
ekolvah <43490155+ekolvah@users.noreply.github.com>
2023-12-29 17:20:53 +0000

    поменял метод загрузки содержимого статей

--- google_scraper.py
+++ google_scraper.py
@@ -1,13 +1,13 @@
 from gnews import GNews
-from newspaper import Article
-import newspaper.article
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
 
+google_news = GNews()
+
 def google_scraper():
     nltk.download("vader_lexicon")
-    keywords = ['nvidia', 'stock']
+    keywords = ['nvidia stock articles']
     date_from = '01/01/20233' 
     date_to = '02/01/20233'
     articles = news_search(keywords, date_from, date_to)
@@ -15,17 +15,10 @@ def google_scraper():
     print(result)
 
 def news_search(keywords, date_from, date_to):
-    google_news = GNews()
+    google_news.period = '1d'
     for keyword in keywords:
       articles = google_news.get_news(keyword)
-      #print(articles[0])
     return articles
-
-def get_article_text(link):
-   article = Article(link)
-   article.download()
-   article.parse()
-   return article.text
 
 def sentiment_analysis(articles):
     analyzer = SentimentIntensityAnalyzer()
@@ -33,18 +26,19 @@ def sentiment_analysis(articles):
     downloaded = 0 
     compound_sum = 0
     for article in articles:
-      try:
         url = article['url']
         #print(url)
-        article_text = get_article_text(url)
-        #print(article_text)
-        downloaded +=1
-        scores = analyzer.polarity_scores(article_text)
-        #print(scores)
-        compound_sum += scores["compound"]
-      except newspaper.article.ArticleException as e:
-         pass
-        
+        article_obj = google_news.get_full_article(url)
+        if article_obj is not None:
+            article_text = google_news.get_full_article(url).text
+            print(str(article['published date']) + " " + str(article['publisher']) + " " + str(article['title']))
+            downloaded +=1
+            scores = analyzer.polarity_scores(article_text)
+            #print(scores)
+            compound_sum += scores["compound"]
+        else:   
+            continue
+    print('Количество статей обработано ' + str(len(articles)))
     compound_mean = compound_sum / len(articles)
     print('Тональность статей: ' + str(compound_mean))
     if compound_mean > 0.05:
