a8a699d73d36b292e209375c81ce06c4a4da36df
ekolvah <43490155+ekolvah@users.noreply.github.com>
2023-12-29 12:02:00 +0000

    поиск и загрузка статей

--- google_scraper.py
+++ google_scraper.py
@@ -1,4 +1,10 @@
-  def google_scraper():
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+from gnews import GNews
+from newspaper import Article
+
+def google_scraper():
     keywords = ['nvidia', 'stock']
     date_from = '01/01/20233' 
     date_to = '02/01/20233'
@@ -6,14 +12,35 @@
     result = sentiment_analysis(articles)
     print(result)
 
-  def news_search(keywords, date_from, date_to):
-    result = ['text']
-    return result
+def news_search(keywords, date_from, date_to):
+    google_news = GNews()
+    for keyword in keywords:
+      articles = google_news.get_news(keyword)
+      #print(articles[0])
+    return articles
 
-  def sentiment_analysis(articles):
+def get_article_text(link):
+   article = Article(link)
+   article.download()
+   article.parse()
+   return article.text
+
+def sentiment_analysis(articles):
     result = ''
+    downloaded = 0 
+    do_not_downloaded = 0
     for article in articles:
-      result += ' ' + article
+      try:
+        get_article_text(article['url'])
+        #print('Article downloaded from ' + article['url'])
+        downloaded +=1
+      except:
+        #print('Article DO NOT downloaded from ' + article['url'])
+        do_not_downloaded +=1
+    percentage = 100*downloaded/(downloaded+do_not_downloaded)
+    print('downloaded ' + str(downloaded))
+    print('do not downloaded ' + str(do_not_downloaded))
+    print("percentage of downloaded articles=" + str(percentage) + '%')
     return result
 
 google_scraper()
