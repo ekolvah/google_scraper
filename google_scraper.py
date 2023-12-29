600a2bdb014ad75695d50c8d6bc4c8165ec3a9f9
ekolvah <43490155+ekolvah@users.noreply.github.com>
2023-12-29 13:31:47 +0000

    добавил анализ тональности статей

--- google_scraper.py
+++ google_scraper.py
@@ -3,8 +3,13 @@ import pandas as pd
 import requests
 from gnews import GNews
 from newspaper import Article
+import newspaper.article
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import sys
 
 def google_scraper():
+    #nltk.download("vader_lexicon")
     keywords = ['nvidia', 'stock']
     date_from = '01/01/20233' 
     date_to = '02/01/20233'
@@ -26,21 +31,34 @@ def get_article_text(link):
    return article.text
 
 def sentiment_analysis(articles):
+    analyzer = SentimentIntensityAnalyzer()
     result = ''
     downloaded = 0 
-    do_not_downloaded = 0
+    compound_sum = 0
     for article in articles:
       try:
-        get_article_text(article['url'])
-        #print('Article downloaded from ' + article['url'])
+        url = article['url']
+        #print(url)
+        article_text = get_article_text(url)
+        #print(article_text)
         downloaded +=1
-      except:
-        #print('Article DO NOT downloaded from ' + article['url'])
-        do_not_downloaded +=1
-    percentage = 100*downloaded/(downloaded+do_not_downloaded)
-    print('downloaded ' + str(downloaded))
-    print('do not downloaded ' + str(do_not_downloaded))
+        scores = analyzer.polarity_scores(article_text)
+        #print(scores)
+        compound_sum += scores["compound"]
+      except newspaper.article.ArticleException as e:
+         pass
+        
+    compound_mean = compound_sum / len(articles)
+    if compound_mean > 0.05:
+        print("Тональность статей: положительная")
+    elif compound_mean < -0.05:
+        print("Тональность статей: отрицательная")
+    else:
+        print("Тональность статей: нейтральная")
+    
+    percentage = round(100*downloaded/len(articles))
     print("percentage of downloaded articles=" + str(percentage) + '%')
+    
     return result
 
 google_scraper()
