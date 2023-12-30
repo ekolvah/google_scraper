1c46d5d8ce7f8883a1ff220981107f7e146be142
ekolvah <43490155+ekolvah@users.noreply.github.com>
2023-12-30 08:47:48 +0000

    добавил сохранение в гуглдок

--- google_scraper.py
+++ google_scraper.py
@@ -2,17 +2,35 @@ from gnews import GNews
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 google_news = GNews()
 
 def google_scraper():
+    worksheet = get_sheet().get_worksheet(0)
     nltk.download("vader_lexicon")
     keywords = ['nvidia stock articles']
     date_from = '01/01/20233' 
     date_to = '02/01/20233'
     articles = news_search(keywords, date_from, date_to)
-    result = sentiment_analysis(articles)
-    print(result)
+    result_of_analysis = sentiment_analysis(articles)
+    save_sentiment_analysis(worksheet, result_of_analysis)
+
+def get_sheet():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    credentials = os.environ['CREDENTIALS']
+    credentials_dict = json.loads(credentials)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1bravbHvEqZDg-5HgVOdH3367Ghs4XOfYNDuEF12LVbg/edit#gid=0')
+    return sheet
+
+def save_sentiment_analysis(worksheet, result_of_analysis):
+    worksheet.update(values=result_of_analysis.values.tolist(), range_name=None)
 
 def news_search(keywords, date_from, date_to):
     google_news.period = '1d'
@@ -21,8 +39,8 @@ def news_search(keywords, date_from, date_to):
     return articles
 
 def sentiment_analysis(articles):
+    data = []
     analyzer = SentimentIntensityAnalyzer()
-    result = ''
     downloaded = 0 
     compound_sum = 0
     for article in articles:
@@ -31,11 +49,11 @@ def sentiment_analysis(articles):
         article_obj = google_news.get_full_article(url)
         if article_obj is not None:
             article_text = google_news.get_full_article(url).text
-            print(str(article['published date']) + " " + str(article['publisher']) + " " + str(article['title']))
             downloaded +=1
             scores = analyzer.polarity_scores(article_text)
             #print(scores)
             compound_sum += scores["compound"]
+            data.append([str(article['published date']), str(article['publisher']), str(article['title'])])
         else:   
             continue
     print('Количество статей обработано ' + str(len(articles)))
@@ -51,6 +69,7 @@ def sentiment_analysis(articles):
     percentage = round(100*downloaded/len(articles))
     print("percentage of downloaded articles=" + str(percentage) + '%')
     
-    return result
+    result_of_analysis = pd.DataFrame(data, columns=['published date', 'publisher', 'title'])
+    return result_of_analysis
 
 google_scraper()
